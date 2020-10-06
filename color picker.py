81eb2fd71019f11f5ea16a9cce78a39d2dbc6259
toushi100 <toushi2009@gmail.com>
2020-10-06 20:19:22 +0200

    mask bug fixed

--- color picker.py
+++ color picker.py
@@ -1,9 +1,9 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
 frameWidth = 640
 frameHeight = 480
-cap = cv.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 
@@ -12,34 +12,38 @@ def empty(a):
     pass
 
 
-cv.namedWindow("HSV")
-cv.resizeWindow("HSV", frameWidth, frameHeight)
-cv.createTrackbar("HUE min", "HSV", 0, 179, empty)
-cv.createTrackbar("HUE max", "HSV", 179, 179, empty)
-cv.createTrackbar("SAT min", "HSV", 0, 255, empty)
-cv.createTrackbar("SAT max", "HSV", 255, 255, empty)
-cv.createTrackbar("VAL min", "HSV", 0, 255, empty)
-cv.createTrackbar("VAL max", "HSV", 255, 255, empty)
+cv2.namedWindow("HSV")
+cv2.resizeWindow("HSV", 640, 240)
+cv2.createTrackbar("HUE Min", "HSV", 0, 179, empty)
+cv2.createTrackbar("HUE Max", "HSV", 179, 179, empty)
+cv2.createTrackbar("SAT Min", "HSV", 0, 255, empty)
+cv2.createTrackbar("SAT Max", "HSV", 255, 255, empty)
+cv2.createTrackbar("VALUE Min", "HSV", 0, 255, empty)
+cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
 
 while True:
-    success, img = cap.read()
-    imgHSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-    h_min = cv.getTrackbarPos("HUE min", "HSV")
-    h_max = cv.getTrackbarPos("HUE max", "HSV")
-    s_min = cv.getTrackbarPos("SAT min", "HSV")
-    s_max = cv.getTrackbarPos("SAT max", "HSV")
-    v_min = cv.getTrackbarPos("VAL min", "HSV")
-    v_max = cv.getTrackbarPos("VAL max", "HSV")
+    success, img = cap.read()
+    imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    h_min = cv2.getTrackbarPos("HUE Min", "HSV")
+    h_max = cv2.getTrackbarPos("HUE Max", "HSV")
+    s_min = cv2.getTrackbarPos("SAT Min", "HSV")
+    s_max = cv2.getTrackbarPos("SAT Max", "HSV")
+    v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
+    v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
+    print(h_min)
+
     lower = np.array([h_min, s_min, v_min])
     upper = np.array([h_max, s_max, v_max])
-    mask = cv.inRange(imgHSV, lower, upper)
-    result = cv.bitwise_and(img, img, mask=mask)
+    mask = cv2.inRange(imgHsv, lower, upper)
+    result = cv2.bitwise_and(img, img, mask=mask)
 
-    mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
     hStack = np.hstack([img, mask, result])
-    cv.imshow("result", hStack)
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('Horizontal Stacking', hStack)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -7,31 +7,13 @@ cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
+#[h_min,s_min,v_min,h_max,s_max,v_max}
+myColors = [
+            [16, 232, 149, 23, 255, 255]]
+myColorValues = [
+                 [0, 255, 255]]
 
-myColors = [[23,67,181,255,62,127]]
-myColorValues = [[51,153,255],          ## BGR
-                 [255,0,255],
-                 [0,255,0],
-                 [255,0,0]]
-
-myPoints = []
-
-
-def findColor(img, myColors, myColorValues):
-    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    count = 0
-    newPoints = []
-    for color in myColors:
-        lower = np.array(color[0:3])
-        upper = np.array(color[3:6])
-        mask = cv2.inRange(imgHSV, lower, upper)
-        x, y = getContours(mask)
-        cv2.circle(imgResult, (x, y), 15, myColorValues[count], cv2.FILLED)
-        if x != 0 and y != 0:
-            newPoints.append([x, y, count])
-        count += 1
-        cv2.imshow(str(color[0]),mask)
-    return newPoints
+myPoints = []  ## [x , y , colorId ]
 
 
 def findColor(img, myColors, myColorValues):
@@ -56,12 +38,12 @@ def getContours(img):
     x, y, w, h = 0, 0, 0, 0
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area > 100:
-            cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+        if area > 500:
+            cv2.drawContours(imgResult, cnt, -1, (0, 0, 0), 3)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
             x, y, w, h = cv2.boundingRect(approx)
-    return x+w//2, y
+    return x + w // 2, y
 
 
 def drawOnCanvas(myPoints, myColorValues):
@@ -82,5 +64,3 @@ while True:
     cv2.imshow("Result", imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-cap.release()
-cv2.destroyAllWindows()
